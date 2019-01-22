87ea64a4c1c383a0f158815a73a5c8cd5e03ad9f
zvzv1919 <zvzv1919@gmail.com>
2019-01-22 16:00:16 -0500

    Basic IO

---
+++ _src/Driver.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Driver {
+    //Temperate class to work as an entry
+    public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog("Insert your equation");
+        JOptionPane.showMessageDialog(null, input);
+    }
+}
