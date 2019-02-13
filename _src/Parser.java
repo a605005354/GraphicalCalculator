59e8f3b05584bb612f58281df22728625f854d98
zvzv1919 <zvzv1919@gmail.com>
2019-02-12 21:31:05 -0500

    Parser test cases and functionTree print
    Added test cases for Parser class and wrote a print feature for FunctionTree to serve the needs

--- _src/Parser.java
+++ _src/Parser.java
@@ -7,8 +7,12 @@ public class Parser {
 
     //TODO
     void parse(){
-
-        functionTree=new FunctionTree(null);
+        Node cn1=new Node(true);
+        Node cn2=new Node(1);
+        Node cn3=new Node(2);
+        Node cn4=Node.combineNode(cn1,cn2,Operator.plus);
+        Node cn5=Node.combineNode(cn4,cn3,Operator.divide);
+        functionTree=new FunctionTree(cn5);
     }
 
     public FunctionTree getFunctionTree() {
