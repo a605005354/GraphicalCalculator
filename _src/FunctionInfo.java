79ad0ae86f4c088f3ea1998385c4bba01589dfbd
zvzv1919 <zvzv1919@gmail.com>
2019-03-01 11:25:14 -0500

    Added some comments, structures

--- _src/FunctionInfo.java
+++ _src/FunctionInfo.java
@@ -1,3 +1,24 @@
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class FunctionInfo {
-    //The highest level data wrapper of a function, should describe the function in every details.
+    ArrayList<ArrayList<Interval>> intervals;
+    ArrayList<ArrayList<FunctionTree>> functionTrees;
+    ArrayList<Color> colors;
+
+    public FunctionInfo(ArrayList<ArrayList<Interval>> intervals, ArrayList<ArrayList<FunctionTree>> functionTrees, ArrayList<Color> colors) {
+        this.intervals = intervals;
+        this.functionTrees = functionTrees;
+        this.colors = colors;
+    }
+
+    public FunctionInfo() {
+        this.intervals=new ArrayList<>();
+        this.functionTrees=new ArrayList<>();
+        this.colors=new ArrayList<>();
+    }
+
+    //The highest level data wrapper of a set of functions, should describe the set of functions in every details.
+    //1st index is the number of function, 2nd index is the number of piece for a piecewise function.
 }
--- _src/Interval.java
+++ _src/Interval.java
@@ -1,3 +1,46 @@
 public class Interval {
     //Represents an interval, open or close, finite or infinite.
+    private float left;
+    private float right;
+    private boolean leftOpen;
+    private boolean rightOpen;
+
+    public Interval(float left, float right, boolean leftOpen, boolean rightOpen) {
+        this.left = left;
+        this.right = right;
+        this.leftOpen = leftOpen;
+        this.rightOpen = rightOpen;
+    }
+
+    public float getLeft() {
+        return left;
+    }
+
+    public float getRight() {
+        return right;
+    }
+
+    public boolean isLeftOpen() {
+        return leftOpen;
+    }
+
+    public boolean isRightOpen() {
+        return rightOpen;
+    }
+
+    public void setLeft(float left) {
+        this.left = left;
+    }
+
+    public void setLeftOpen(boolean leftOpen) {
+        this.leftOpen = leftOpen;
+    }
+
+    public void setRight(float right) {
+        this.right = right;
+    }
+
+    public void setRightOpen(boolean rightOpen) {
+        this.rightOpen = rightOpen;
+    }
 }
